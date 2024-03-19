6143966b6b48cee529b26dd57664352f23264f30
liuzhenhui123 <59080601+liuzhenhui123@users.noreply.github.com>
2024-03-19 11:43:02 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/1.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Awrt01.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
